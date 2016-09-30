9297fa469ed0c7cb9d67b8d4bcbe9e0ed2991664
crynobone <crynobone@gmail.com>
2016-09-30 09:37:12 +0800

    Add basic constant.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Request.php
+++ src/Request.php
@@ -10,6 +10,15 @@ abstract class Request
     use WithSanitizer;
 
     /**
+     * Constants for base HTTP Method.
+     */
+    const GET_HTTP = 'GET';
+    const POST_HTTP = 'POST';
+    const PUT_HTTP = 'PUT';
+    const PATCH_HTTP = 'PATCH';
+    const DELETE_HTTP = 'DELETE';
+
+    /**
      * Version namespace.
      *
      * @var string
