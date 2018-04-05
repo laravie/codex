6da143e8c48ba5a8915804f239be5d39dcea151c
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-05 13:34:34 +0800

    If request is GET, body content is irrelevant.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Testing/FakeRequest.php
+++ src/Testing/FakeRequest.php
@@ -89,6 +89,10 @@ class FakeRequest
      */
     public function call(string $method = 'GET', $headers = [], $body = ''): self
     {
+        if ($method === 'GET') {
+            $body = m::any();
+        }
+
         $this->http->shouldReceive('send')
                 ->with($method, m::type(Uri::class), $headers, $body)
                 ->andReturnUsing(function ($m, $u, $h, $b) {
