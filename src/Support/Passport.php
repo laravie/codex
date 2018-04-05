ab02fc4a95a6faa50357b8ab01fbe53f34dbaa4a
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-05 10:19:33 +0800

    Add passport helper.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Support/Passport.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Laravie\Codex\Support;
+
+trait Passport
+{
+    /**
+     * API Client ID.
+     *
+     * @var string|null
+     */
+    protected $clientId;
+
+    /**
+     * API Client Secret.
+     *
+     * @var string|null
+     */
+    protected $clientSecret;
+
+    /**
+     * API Access Token.
+     *
+     * @var string|null
+     */
+    protected $accessToken;
+
+    /**
+     * Get Client ID.
+     *
+     * @return string|null
+     */
+    public function getClientId(): ?string
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * Set Client ID.
+     *
+     * @param  string|null  $clientId
+     *
+     * @return $this
+     */
+    public function setClientId(?string $clientId): self
+    {
+        $this->clientId = $clientId;
+
+        return $this;
+    }
+
+    /**
+     * Get Client Secret.
+     *
+     * @return string|null
+     */
+    public function getClientSecret(): ?string
+    {
+        return $this->clientSecret;
+    }
+
+    /**
+     * Set Client Secret.
+     *
+     * @param  string|null  $clientSecret
+     *
+     * @return $this
+     */
+    public function setClientSecret(?string $clientSecret): self
+    {
+        $this->clientSecret = $clientSecret;
+
+        return $this;
+    }
+
+    /**
+     * Get access token.
+     *
+     * @return string|null
+     */
+    public function getAccessToken(): ?string
+    {
+        return $this->accessToken;
+    }
+
+    /**
+     * Set access token.
+     *
+     * @param string|null $accessToken
+     *
+     * @return $this
+     */
+    public function setAccessToken(?string $accessToken): self
+    {
+        $this->accessToken = $accessToken;
+
+        return $this;
+    }
+}
