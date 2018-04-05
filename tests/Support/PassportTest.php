53cbaf1bbc11cef8853f12316fbf61ec1f790fef
Mior Muhammad Zaki <crynobone@gmail.com>
2018-04-05 11:02:38 +0800

    Add tests for passport.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Support/PassportTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Laravie\Codex\TestCase\Support;
+
+use PHPUnit\Framework\TestCase;
+use Laravie\Codex\Support\Passport;
+
+class PassportTest extends TestCase
+{
+    /** @test */
+    public function it_can_uses_client_id()
+    {
+        $passport = new class() {
+            use Passport;
+        };
+
+        $this->assertNull($passport->getClientId());
+
+        $passport->setClientId('homestead');
+
+        $this->assertSame('homestead', $passport->getClientId());
+    }
+
+    /** @test */
+    public function it_can_uses_client_secret()
+    {
+        $passport = new class() {
+            use Passport;
+        };
+
+        $this->assertNull($passport->getClientSecret());
+
+        $passport->setClientSecret('secret');
+
+        $this->assertSame('secret', $passport->getClientSecret());
+    }
+
+    /** @test */
+    public function it_can_uses_access_token()
+    {
+        $passport = new class() {
+            use Passport;
+        };
+
+        $this->assertNull($passport->getAccessToken());
+
+        $passport->setAccessToken('AckfSECXIvnK5r28GVIWUAxmbBSjTsmF');
+
+        $this->assertSame('AckfSECXIvnK5r28GVIWUAxmbBSjTsmF', $passport->getAccessToken());
+    }
+}
