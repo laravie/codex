928201283af99acfaf221bbfd8ad420771f16240
Mior Muhammad Zaki <crynobone@gmail.com>
2018-12-14 22:01:40 +0800

    Update typehint.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Request.php
+++ src/Request.php
@@ -20,7 +20,7 @@ abstract class Request implements Contracts\Request
     /**
      * The Codex client.
      *
-     * @var \Laravie\Codex\Client
+     * @var \Laravie\Codex\Contracts\Client
      */
     protected $client;
 
