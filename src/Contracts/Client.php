b0acd999eeff7900274983e4235c72083064990d
Mior Muhammad Zaki <crynobone@gmail.com>
2018-12-14 22:05:17 +0800

    Reduce contract.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Contracts/Client.php
+++ src/Contracts/Client.php
@@ -7,40 +7,6 @@ use Psr\Http\Message\StreamInterface;
 interface Client
 {
     /**
-     * Use custom API Endpoint.
-     *
-     * @param  string  $endpoint
-     *
-     * @return $this
-     */
-    public function useCustomApiEndpoint(string $endpoint);
-
-    /**
-     * Use different API version.
-     *
-     * @param  string  $version
-     *
-     * @throws \InvalidArgumentException
-     *
-     * @return $this
-     */
-    public function useVersion(string $version);
-
-    /**
-     * Get API endpoint URL.
-     *
-     * @return string
-     */
-    public function getApiEndpoint(): ?string;
-
-    /**
-     * Get API default version.
-     *
-     * @return string
-     */
-    public function getApiVersion(): string;
-
-    /**
      * Send the HTTP request.
      *
      * @param  string  $method
