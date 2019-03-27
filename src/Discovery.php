2e6039af263ba92d0648b1f1c4d468897a5c8ba8
Mior Muhammad Zaki <crynobone@gmail.com>
2019-03-27 14:30:05 +0800

    Extends Discovery.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Discovery.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Laravie\Codex;
+
+final class Discovery extends \Laravie\Codex\Common\Discovery
+{
+    //
+}
---
+++ tests/DiscoveryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Laravie\Codex\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Laravie\Codex\Discovery;
+
+class DiscoveryTest extends TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    protected function tearDown(): void
+    {
+        Discovery::flush();
+    }
+
+    /** @test */
+    public function it_can_set_client()
+    {
+        $http = Discovery::client();
+
+        $this->assertInstanceOf(\Http\Client\HttpClient::class, $http);
+
+        $this->assertSame($http, Discovery::client());
+    }
+
+    /** @test */
+    public function it_can_refresh_client()
+    {
+        $http = Discovery::client();
+
+        $this->assertNotSame($http, Discovery::refreshClient());
+    }
+
+    /** @test */
+    public function it_can_override_client()
+    {
+        $http1 = Discovery::client();
+        $http2 = Discovery::override($override = Discovery::make());
+
+        $this->assertNotSame($http1, $http2);
+        $this->assertSame($override, $http2);
+    }
+}
